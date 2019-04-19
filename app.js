69d464ad39572ad30ad403afb9c658e813571846
Micah Sturm <Sturm.mx@gmail.com>
2019-04-18 19:23:56 -0500

    First commit

--- app.js
+++ app.js
@@ -6,12 +6,31 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const MongoClient = require('mongodb').MongoClient;
 
 var app = express();
+
+//TO CONNECT MONGODB 
+const uri = "mongodb+srv://application:ksNwA9NnUySKuFtj@greensteak-c0hau.mongodb.net/test?retryWrites=true"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
