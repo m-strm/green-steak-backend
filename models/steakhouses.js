a4706408707c83d35beaafd4d14db67f549ba4b3
Micah Sturm <Sturm.mx@gmail.com>
2019-04-20 12:54:15 -0500

    Mogoose Schema

--- models/steakhouses.js
+++ models/steakhouses.js
@@ -1,15 +1,1 @@
-'use strict';
-module.exports = (mongodb, DataTypes) => {
-  const steakhouses = mongodb.define(
-    'steakhouses',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      Name: DataTypes.STRING
-    }
-  );
-  return steakhouses;
-};
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+var steakhouseSchema = new Schema({
+  id: Number,
+  name: String
+})
+
+var Steakhouses = mongoose.model('steakhouses', steakhouseSchema)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
